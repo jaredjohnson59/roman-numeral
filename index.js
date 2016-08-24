3eb6c78a568a1c1c5014079c6c82b8efd0779202
jaredjohnson59 <jaredjohnson59@gmail.com>
2016-08-24 13:17:57 +0100

    Create basic roman numeral class

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var chai = require('chai');
 var should = chai.should();
+var expect = chai.expect;
 
 describe('Number', function(){
-  it('should return number');
+  it('should return number', function()
+  {
+    var number = RomanNumber(20);
+    expect(number.toInt()).to.equal(20);
+  });
   it('should be between 1 and 3999');
   it('should not be null');
   it('should not return an error');
@@ -14,3 +19,37 @@ describe('Roman Numeral', function(){
   it('should not be null');
   it('should not return an error');
 });
+
+function RomanNumber(pNumber)
+{
+
+	if(pNumber !== undefined)
+	{
+	this.number = pNumber;
+	}
+	else
+	{
+		throw new Error("Please enter number");
+	}
+
+
+	this.toInt = function()
+	{
+		if (Number.isInteger(this.number)) {
+		return this.number;
+		}
+		else
+		{
+			console.log("This is a string");
+			return this.number;
+		}
+	};
+
+	this.toString = function()
+	{
+		if (Number.isInteger(this.number)) {
+		return this.number;
+		}
+
+	};
+}
