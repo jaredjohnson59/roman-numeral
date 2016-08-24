0df8ccb99e126ce9633a813169d9b2bdb2a9de9b
jaredjohnson59 <jaredjohnson59@gmail.com>
2016-08-24 13:37:31 +0100

    Adding Functionality to Roman Numeral Class

--- index.js
+++ index.js
@@ -6,9 +6,15 @@ describe('Number', function(){
   it('should return number', function()
   {
     var number = RomanNumber(20);
-    expect(number.toInt()).to.equal(20);
+    expect(number.toInt()).not.to.be.NaN;
   });
-  it('should be between 1 and 3999');
+
+  it('should be between 1 and 3999', function()
+  {
+    var number = RomanNumber(4000);
+    expect(number.toInt()).to.eql(new Error('Number must be between 1 and 3999'));
+  });
+
   it('should not be null');
   it('should not return an error');
 });
@@ -22,15 +28,23 @@ describe('Roman Numeral', function(){
 
 function RomanNumber(pNumber)
 {
+	if (!(this instanceof RomanNumber)){
+        return new RomanNumber(pNumber);
+   }
 
-	if(pNumber !== undefined)
+   //Checks if user enter number
+	if(pNumber !== undefined && pNumber >=1 && pNumber <= 3999)
 	{
 	this.number = pNumber;
 	}
-	else
+	else if(pNumber === undefined)
 	{
 		throw new Error("Please enter number");
 	}
+  else
+  {
+      throw new Error("Number must be between 1 and 3999");
+  }
 
 
 	this.toInt = function()
