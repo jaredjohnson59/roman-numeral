981c3645dd1cce2d6ec46c12a428a9081eb4049d
jaredjohnson59 <jaredjohnson59@gmail.com>
2016-08-24 13:00:15 +0100

    Create tests to be run

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 var chai = require('chai');
 var should = chai.should();
+
+describe('Number', function(){
+  it('should return number');
+  it('should be between 1 and 3999');
+  it('should not be null');
+  it('should not return an error');
+});
+
+describe('Roman Numeral', function(){
+  it('should return roman numeral');
+  it('should be between 1 and 3999');
+  it('should not be null');
+  it('should not return an error');
+});
